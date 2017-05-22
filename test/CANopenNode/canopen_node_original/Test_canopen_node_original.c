d6faea61c083096cb1c68064a1dd67c3b09f7512
Izidor Makuc <izidor@makuc.info>
2017-05-22 16:14:50 +0200

    Adding test environment for CANopenNode
    No tests are implemented atm. Only the directories and Makefile is
    added so CANopenNode can be compiled, so testing and modifications can
    be made...

---
+++ test/CANopenNode/canopen_node_original/Test_canopen_node_original.c
@@ -0,0 +1,24 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//#include "utils.h"
+
+#include "unity.h"
+
+void setUp(void)
+{
+//        test_plogger_reset();
+}
+
+void tearDown(void)
+{
+        
+}
+
+void test_simple_type(void)
+{
+//        
+//        TEST_ASSERT_EQUAL_UINT16(2, l);
+}
